b74bb3de28f34f5a5fcfdd84f4cdb161409188aa
sheenazien8 <sheenazien08@gmail.com>
2024-11-21 22:20:42 +0700

    fix format localized date funciton

--- app/Helpers/DateHelper.php
+++ app/Helpers/DateHelper.php
@@ -1,12 +1,11 @@
 <?php
 
-use App\Models\Closing;
 use Carbon\Carbon;
 
-if (!function_exists('get_months')) {
+if (! function_exists('get_months')) {
     function get_months()
     {
-        return array(
+        return [
             'Januari',
             'Februari',
             'Maret',
@@ -19,15 +18,15 @@ if (!function_exists('get_months')) {
             'Oktober',
             'November',
             'Desember',
-        );
+        ];
     }
 }
 
-if (!function_exists('get_month_simple')) {
+if (! function_exists('get_month_simple')) {
     function get_month_simple($month)
     {
-        $month = ((integer) $month) - 1;
-        $array =  array(
+        $month = ((int) $month) - 1;
+        $array = [
             'JAN',
             'FEB',
             'MAR',
@@ -40,112 +39,118 @@ if (!function_exists('get_month_simple')) {
             'OKT',
             'NOV',
             'DES',
-        );
+        ];
 
         return $array[$month];
     }
 }
 
-if (!function_exists('get_month_name')) {
+if (! function_exists('get_month_name')) {
     function get_month_name($month)
     {
         $m = $month - 1;
+
         return get_months()[$m] ?? 'Undefined';
     }
 }
 
-if (!function_exists('get_month_romawi')) {
+if (! function_exists('get_month_romawi')) {
     function get_month_roman()
     {
-        switch (date('m')){
-            case 1;
+        switch (date('m')) {
+            case 1:
                 return 'I';
-            case 2;
+            case 2:
                 return 'II';
-            case 3;
+            case 3:
                 return 'III';
-            case 4;
+            case 4:
                 return 'IV';
-            case 5;
+            case 5:
                 return 'V';
-            case 6;
+            case 6:
                 return 'VI';
-            case 7;
+            case 7:
                 return 'VII';
-            case 8;
+            case 8:
                 return 'VIII';
-            case 9;
+            case 9:
                 return 'IX';
-            case 10;
+            case 10:
                 return 'X';
-            case 11;
+            case 11:
                 return 'XI';
-            case 12;
+            case 12:
                 return 'XII';
         }
     }
 }
 
-if (!function_exists('parse_date_format')) {
-    function parse_date_format($date) {
+if (! function_exists('parse_date_format')) {
+    function parse_date_format($date)
+    {
         return Carbon::parse($date)->format('Y-m-d');
     }
 }
 
-if (!function_exists('parse_date')) {
+if (! function_exists('parse_date')) {
     function parse_date($date = null)
     {
-        if($date == null){
+        if ($date == null) {
             return '-';
         }
         \Carbon\Carbon::setLocale('id');
-//        $date = \Carbon\Carbon::parse($date)->formatLocalized("%d %B %Y");
+        //        $date = \Carbon\Carbon::parse($date)->formatLocalized("%d %B %Y");
         $date = \Carbon\Carbon::parse($date);
+
         return $date->format('d').'-'.get_month_simple($date->format('m')).'-'.$date->format('Y');
     }
 }
 
-if (!function_exists('parse_date_full')) {
+if (! function_exists('parse_date_full')) {
     function parse_date_full($date = null)
     {
-        if($date == null){
+        if ($date == null) {
             return '-';
         }
         \Carbon\Carbon::setLocale('id');
-//        $date = \Carbon\Carbon::parse($date)->formatLocalized("%d %B %Y");
+        //        $date = \Carbon\Carbon::parse($date)->formatLocalized("%d %B %Y");
         $date = \Carbon\Carbon::parse($date);
+
         return $date->format('d').' '.get_month_name($date->format('m')).' '.$date->format('Y');
     }
 }
 
-if (!function_exists('parse_date_time')) {
+if (! function_exists('parse_date_time')) {
     function parse_date_time($date = null)
     {
-        if($date == null){
+        if ($date == null) {
             return '-';
         }
         \Carbon\Carbon::setLocale('id');
-//        $date = \Carbon\Carbon::parse($date)->formatLocalized("%d %B %Y, %I:%M:%S %p");
+        //        $date = \Carbon\Carbon::parse($date)->formatLocalized("%d %B %Y, %I:%M:%S %p");
         $date = \Carbon\Carbon::parse($date);
-        return $date->format('d').'-'.get_month_simple($date->format('m')).'-'.$date->format('Y'). ', '.\Carbon\Carbon::parse($date)->formatLocalized("%H:%M:%S");
-//        return $date;
+
+        return $date->format('d').'-'.get_month_simple($date->format('m')).'-'.$date->format('Y').', '.Carbon::parse($date)->format('%H:%M:%S');
+        //        return $date;
     }
 }
 
-if (!function_exists('parse_date_time_full')) {
+if (! function_exists('parse_date_time_full')) {
     function parse_date_time_full($date = null)
     {
-        if($date == null){
+        if ($date == null) {
             return '-';
         }
         \Carbon\Carbon::setLocale('id');
 
         $date = \Carbon\Carbon::parse($date);
-        return $date->format('d').' '.get_month_name($date->format('m')).' '.$date->format('Y'). ', '.\Carbon\Carbon::parse($date)->formatLocalized("%H:%M:%S");
+
+        return $date->format('d').' '.get_month_name($date->format('m')).' '.$date->format('Y').', '.\Carbon\Carbon::parse($date)->formatLocalized('%H:%M:%S');
     }
 }
 
-if (!function_exists('parse_month_year')) {
+if (! function_exists('parse_month_year')) {
     function parse_month_year($date = null)
     {
         if ($date == null) {
@@ -155,11 +160,12 @@ if (!function_exists('parse_month_year')) {
         \Carbon\Carbon::setLocale('id');
 
         $date = \Carbon\Carbon::parse($date);
+
         return get_month_name($date->format('m')).' '.$date->format('Y');
     }
 }
 
-if (!function_exists('parse_year')) {
+if (! function_exists('parse_year')) {
     function parse_year($date = null)
     {
         if ($date == null) {
@@ -169,26 +175,27 @@ if (!function_exists('parse_year')) {
         \Carbon\Carbon::setLocale('id');
 
         $date = \Carbon\Carbon::parse($date);
+
         return $date->format('Y');
     }
 }
 
-if (!function_exists('parse_start_and_end_date')) {
+if (! function_exists('parse_start_and_end_date')) {
     function parse_start_and_end_date($start, $end)
     {
         $start = Carbon::parse($start)->format('m/d/Y');
         $end = Carbon::parse($end)->format('m/d/Y');
 
-        return $start ." - ". $end;
+        return $start.' - '.$end;
     }
 }
 
-if (!function_exists('get_years')) {
+if (! function_exists('get_years')) {
     function get_years()
     {
         $year = collect();
 
-        for($i = 2010; $i < 2050; $i ++){
+        for ($i = 2010; $i < 2050; $i++) {
             $year->push($i);
         }
 
@@ -197,21 +204,22 @@ if (!function_exists('get_years')) {
     }
 }
 
-if (!function_exists('get_start_and_end_date')) {
+if (! function_exists('get_start_and_end_date')) {
     function get_start_and_end_date($dates)
     {
         $range = explode(' - ', $dates);
 
         $start = Carbon::createFromFormat('m/d/Y', $range[0])->toDateString();
         $end = Carbon::createFromFormat('m/d/Y', $range[1])->toDateString();
+
         return [
             'start_date' => $start,
-            'end_date' => $end
+            'end_date' => $end,
         ];
     }
 }
 
-if (!function_exists('get_date_ranges')) {
+if (! function_exists('get_date_ranges')) {
     function get_date_ranges($dates)
     {
         $range = explode(' - ', $dates);
@@ -220,7 +228,7 @@ if (!function_exists('get_date_ranges')) {
 
         $dates = [];
 
-        for($date = $start; $date->lte($end); $date->addDay()) {
+        for ($date = $start; $date->lte($end); $date->addDay()) {
             $dates[] = $date->format('Y-m-d');
         }
 
@@ -228,7 +236,7 @@ if (!function_exists('get_date_ranges')) {
     }
 }
 
-if (!function_exists('get_date_array')) {
+if (! function_exists('get_date_array')) {
     function get_date_array($start, $end)
     {
         $start = Carbon::parse($start);
@@ -236,7 +244,7 @@ if (!function_exists('get_date_array')) {
 
         $dates = [];
 
-        for($date = $start; $date->lte($end); $date->addDay()) {
+        for ($date = $start; $date->lte($end); $date->addDay()) {
             $dates[] = $date->format('Y-m-d');
         }
 
