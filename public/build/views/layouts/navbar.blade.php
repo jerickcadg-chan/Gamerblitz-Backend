7ffd5e9b952104a2785f933cb1b120555289bb7b
sheenazien8 <sheenazien08@gmail.com>
2025-01-24 17:31:20 +0700

    update logo to follow the client

--- public/build/views/layouts/navbar.blade.php
+++ public/build/views/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
   <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-    <a class="navbar-brand brand-logo" href="index.html"><img src="{{ asset('build/img/header.png') }}" alt="logo" /></a>
-    <a class="navbar-brand brand-logo-mini" href="index.html"><img src="{{ asset('build/img/header.png') }}" alt="logo" /></a>
+    <a class="navbar-brand brand-logo" href="index.html"><img src="{{ client()->logo }}" alt="logo" /></a>
+    <a class="navbar-brand brand-logo-mini" href="index.html"><img src="{{ client()->logo }}" alt="logo" /></a>
   </div>
   <div class="navbar-menu-wrapper d-flex align-items-stretch">
     <button class="navbar-toggler align-self-center" type="button" id="navbar-toggler" data-toggle="minimize" onclick="navbarToggler()">
