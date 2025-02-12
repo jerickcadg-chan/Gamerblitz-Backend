ff059cd58b7ab1f429b9fb1d6102ac1242b63d27
sheenazien8 <sheenazien08@gmail.com>
2025-02-12 07:26:37 +0700

    add user level constant

---
+++ app/Constants/UserLevel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Constants;
+
+use Sheenazien8\Konstantiq\ConstanstAbstraction;
+
+class UserLevel extends ConstanstAbstraction
+{
+    const SILVER = 'silver';
+    const GOLD = 'gold';
+    const PLATINUM = 'platinum';
+    const DIAMOND = 'diamond';
+}
