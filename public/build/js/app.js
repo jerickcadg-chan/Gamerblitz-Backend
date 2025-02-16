4658140ce2ce6250f04c2425fc510190be7d7fd1
sheenazien8 <sheenazien08@gmail.com>
2025-02-16 15:12:01 +0700

    delete build file

--- public/build/js/app.js
+++
@@ -1,5 +0,0 @@
-import './vendor.bundle.base.js';
-import './misc.js';
-import './hoverable-collapse.js';
-import '../css/materialdesignicons.min.css';
-
--- public/build/lang/id/order.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-
-    'out_of_stock' => 'Stok produk ini sudah habis',
-    'no_balance' => 'Saldo anda tidak cukup',
-
-];
