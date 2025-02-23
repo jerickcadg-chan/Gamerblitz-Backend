8f42808ae5dbf285d2ebcbde50a6232431c398e0
sheenazien8 <sheenazien08@gmail.com>
2025-02-23 12:09:54 +0700

    website level

---
+++ app/Constants/WebsiteLevel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Constants;
+
+use Sheenazien8\Konstantiq\ConstanstAbstraction;
+
+class WebsiteLevel extends ConstanstAbstraction
+{
+    const BASIC = "basic";
+    const PREMIUM = "premium";
+    const ULTIMATE = "ultimate";
+}
+
