94b2fd649ffcb0e49ffecfa39f2d7aeb9bada894
sheenazien8 <sheenazien08@gmail.com>
2025-03-20 05:29:33 +0700

    fix where by client

--- app/Http/Controllers/Api/SliderController.php
+++ app/Http/Controllers/Api/SliderController.php
@@ -3,16 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Slider;
-use Illuminate\Http\Request;
-use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\DB;
 use App\Transformers\SliderTransformer;
+use Illuminate\Routing\Controller;
 
 class SliderController extends Controller
 {
     public function index()
     {
-        $sliders = Slider::all();
+        $sliders = Slider::whereByClient()
+            ->get();
 
         return api_status_ok(transformer($sliders, new SliderTransformer));
     }
