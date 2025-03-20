84d26d10a3f2d2fb628a58da9df32513acdd97b2
sheenazien8 <sheenazien08@gmail.com>
2025-03-20 21:51:12 +0700

    back to string

--- app/Transformers/FlashSaleTransformer.php
+++ app/Transformers/FlashSaleTransformer.php
@@ -38,7 +38,7 @@ class FlashSaleTransformer extends TransformerAbstract
                 return [
                     'id' => $item->id,
                     'slug' => $item->productItem?->product?->slug,
-                    'flash_price' => (double) $item->price,
+                    'flash_price' => $item->price,
                     'stock' => (float) $item->stock,
                     'real_price' => $item->productItem?->real_price,
                     'product_name' => $item->productItem?->product?->name,
