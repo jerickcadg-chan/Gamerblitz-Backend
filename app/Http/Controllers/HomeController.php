71a75fb7c85c9da07c5717554b33b7cfde2b6544
Yeri <mail@yeripratama.com>
2025-03-21 07:32:02 +0700

    feat(dashboard): statistic past week order

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
@@ -22,39 +23,69 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(): View
     {
         $selectedMonth = request('month') ?? today()->format('m');
         $selectedYear = request('year') ?? today()->format('Y');
 
-        $order = Order::whereClient()
-            ->whereMonth('created_at', $selectedMonth)
-            ->whereYear('created_at', $selectedYear)
+        $orderToday = $this->getOrderToday();
+        $orderSum = $this->getOrderSum($selectedMonth, $selectedYear);
+        $orderPastWeek = $this->getOrderPastWeek();
+
+        $data = compact('orderSum', 'orderToday', 'orderPastWeek', 'selectedYear', 'selectedMonth');
+        return view('home', $data);
+    }
+
+    protected function getOrderPastWeek()
+    {
+        $query = Order::whereClient()
+            ->where('order_status', Order::DONE)
+            ->selectRaw('DATE(created_at) as date, SUM(total_price) as total_price, SUM(total_income) as total_income')
+            ->whereBetween('created_at', [now()->subWeek(), now()])
+            ->groupBy('date')
+            ->orderBy('date');
+        $turnover = $query->pluck('total_price');
+        $profit = $query->pluck('total_income');
+        $days = $query->pluck('date');
+        return compact('turnover', 'profit', 'days');
+    }
+
+    protected function getOrderSum($month, $year)
+    {
+        $orderSumQuery = Order::whereClient()
+            ->whereMonth('created_at', $month)
+            ->whereYear('created_at', $year)
             ->where('order_status', Order::DONE);
 
-        // $progress = Order::whereClient()
-        //     ->select(
-        //         DB::raw('DATE(created_at) as date'),
-        //         DB::raw('SUM(total_income) as total_income'),
-        //         DB::raw('count(*) as count')
-        //     )
-        //     ->settlement()
-        //     ->whereMonth('created_at', $month)
-        //     ->whereYear('created_at', $year)
-        //     ->groupBy('date')
-        //     ->get();
-
-        $turnover = $order->sum('total_price');
-        $profit = $order->sum('total_income');
+        $turnover = $orderSumQuery->sum('total_price');
+        $profit = $orderSumQuery->sum('total_income');
         $profitPercent = $turnover === 0 ? 0 : round(($profit / $turnover) * 100);
         $orderSum = [
-            'total' => $order->count(),
+            'total' => $orderSumQuery->count(),
             'turnover' => $turnover,
             'profit' => $profit,
             'profitPercent' => $profitPercent,
         ];
 
-        $data = compact('orderSum', 'selectedYear', 'selectedMonth');
-        return view('home', $data);
+        return $orderSum;
+    }
+    /**
+     * @return array<string,mixed>
+     */
+    protected function getOrderToday()
+    {
+        $orderTodayQuery = Order::whereClient()
+            ->where('created_at', '=', today())
+            ->where('order_status', Order::DONE);
+
+
+        $turnoverToday = $orderTodayQuery->sum('total_price');
+        $profitToday = $orderTodayQuery->sum('total_income');
+        $orderToday = [
+            'total' => $orderTodayQuery->count(),
+            'turnover' => $turnoverToday,
+            'profit' => $profitToday,
+        ];
+        return $orderToday;
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,9 @@
 ])
 
 @section('content')
-<div class="page-header">
-  <h3 class="page-title"> Dashboard</h3>
-</div>
-
 <form>
   <div class="row g-2 mb-4">
-    <div class="col-lg-5">
+    <div class="col-xl-5">
       <label for="month-input">Bulan</label>
       <select id="month-input" class="form-control" name="month" autocomplete="off">
         @foreach(get_months() as $monthIndex => $month)
@@ -17,7 +13,7 @@
         @endforeach
       </select>
     </div>
-    <div class="col-lg-5">
+    <div class="col-xl-5">
       <label for="year-input">Tahun</label>
       <select id="year-input" class="form-control" name="year" autocomplete="off">
         @foreach(get_years_reversed() as $year)
@@ -25,73 +21,83 @@
         @endforeach
       </select>
     </div>
-    <div class="col-lg-2 d-flex align-items-center">
+    <div class="col-xl-2 d-flex align-items-center">
       <button type="submit" class="btn btn-sm btn-primary">Filter</button>
     </div>
   </div>
 </form>
 
-<div class="col-lg-12">
-  <div class="row">
-    <div class="col-md-4">
-      <div class="card bg-gradient-danger card-img-holder text-white">
-        <div class="card-body">
-          <h4 class="font-weight-normal mb-3">Jumlah Transaksi <i class="mdi mdi-chart-line mdi-24px float-right"></i>
-          </h4>
-          <h2>{{ $orderSum['total'] }}</h2>
-        </div>
+<div class="row mb-4">
+  <div class="col-md-4">
+    <div class="card bg-gradient-danger card-img-holder text-white">
+      <div class="card-body">
+        <h4 class="font-weight-normal mb-3">Jumlah Transaksi <i class="mdi mdi-chart-line mdi-24px float-right"></i>
+        </h4>
+        <h2>{{ $orderSum['total'] }}</h2>
       </div>
     </div>
-    <div class="col-md-4">
-      <div class="card bg-gradient-info card-img-holder text-white">
-        <div class="card-body">
-          <h4 class="font-weight-normal mb-3">Omset <i class="mdi mdi-bookmark-outline mdi-24px float-right"></i>
-          </h4>
-          <h2>{{ rp_format($orderSum['turnover']) }}</h2>
-        </div>
+  </div>
+  <div class="col-md-4">
+    <div class="card bg-gradient-info card-img-holder text-white">
+      <div class="card-body">
+        <h4 class="font-weight-normal mb-3">Omset <i class="mdi mdi-bookmark-outline mdi-24px float-right"></i>
+        </h4>
+        <h2>{{ rp_format($orderSum['turnover']) }}</h2>
       </div>
     </div>
-    <div class="col-md-4">
-      <div class="card bg-gradient-success card-img-holder text-white">
-        <div class="card-body">
-          <h4 class="font-weight-normal mb-3">Profit <i class="mdi mdi-diamond mdi-24px float-right"></i>
-          </h4>
-          <h2>{{ rp_format($orderSum['profit']) }} <small>({{ $orderSum['profitPercent'] }}%)</small></h2>
-        </div>
+  </div>
+  <div class="col-md-4">
+    <div class="card bg-gradient-success card-img-holder text-white">
+      <div class="card-body">
+        <h4 class="font-weight-normal mb-3">Profit <i class="mdi mdi-diamond mdi-24px float-right"></i>
+        </h4>
+        <h2>{{ rp_format($orderSum['profit']) }} <small>({{ $orderSum['profitPercent'] }}%)</small></h2>
       </div>
     </div>
   </div>
 </div>
 
-{{-- <div class="col-md-6 mt-4">
-  <div class="card">
-    <div class="card-header">
-      <b>Progres Harian</b>
-    </div>
-    <div class="card-body">
-      <table class="table table-hover table-bordered">
-        <thead class="bg-gradient-danger text-white">
-          <tr>
-            <th>Tanggal</th>
-            <th>Est. Laba</th>
-            <th>Jumlah Pesanan</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach ($progress as $p)
-          <tr>
-            <td>{{ parse_date($p->date) }}</td>
-            <td align="right">{{ rp_format($p->total_income) }}</td>
-            <td>{{ currency_format($p->count) }}</td>
-          </tr>
-          @endforeach
-          <tr>
-            <td colspan="2" align="right">{{ rp_format($progress->sum('total_income')) }}</td>
-            <td>{{ $progress->sum('count') }}</td>
-          </tr>
-        </tbody>
-      </table>
+<div class="row">
+  <div class="col-md-8">
+    <div id="last-week-chart"></div>
+  </div>
+  <div class="col-md-4">
+    <div class="card bg-gradient-primary card-img-holder text-white">
+      <div class="card-body">
+        <h4 class="mb-0 font-weight-normal">Jumlah Transaksi Hari Ini</h4>
+        <h3 class="mb-4">{{ rp_format($orderToday['total']) }}</h3>
+        <h4 class="mb-0 font-weight-normal">Omset Hari ini</h4>
+        <h3 class="mb-4">{{ rp_format($orderToday['turnover']) }}</h3>
+        <h4 class="mb-0 font-weight-normal">Profit Hari Ini</h4>
+        <h3 class="mb-4">{{ rp_format($orderToday['profit']) }}</h3>
+      </div>
     </div>
   </div>
-</div> --}}
+</div>
+
 @endsection
+
+@push('js')
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+<script>
+var options = {
+  chart: {
+    type: 'area'
+  },
+  series: [{
+    name: 'Omset',
+    data: {{ Js::from($orderPastWeek['turnover']) }},
+  }, {
+    name: 'Profit',
+    data: {{ Js::from($orderPastWeek['profit']) }}
+  }],
+  xaxis: {
+    categories: {{ Js::from($orderPastWeek['days']) }}
+  }
+}
+
+var chart = new ApexCharts(document.querySelector("#last-week-chart"), options);
+
+chart.render();
+</script>
+@endpush
