ddadb2546db63af5ba33e3034e3606e02658e165
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2025-08-16 13:12:06 +0700

    feat: change primary color

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/js/app.js',
                 'resources/css/style.css',
                 'resources/sass/app.scss',
             ],
