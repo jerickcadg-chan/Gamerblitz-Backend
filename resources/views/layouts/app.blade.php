1c92fb0aeacaf69b5b528bf8925e3245dcd7562f
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2025-08-28 07:56:44 +0700

    feat: fix asset url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <script src="{{ asset('vendors/select2/select2.min.js') }}"></script>
 
     <!-- Shortcut icon -->
-    <link rel="shortcut icon" href="{{ asset('img/gpds_icon.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('/setting/'.get_setting('favicon')['value']) }}" />
 </head>
 <body>
     @auth
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
         <li class="nav-item nav-profile">
             <a href="#" class="nav-link">
                 <div class="nav-profile-image">
-                    <img src="{{ get_setting('favicon')['value'] }}" alt="profile">
+                    <img src="{{ asset('/storage/'.get_setting('favicon')['value']) }}" alt="profile">
                     <span class="login-status online"></span>
                 </div>
                 <div class="nav-profile-text d-flex flex-column">
