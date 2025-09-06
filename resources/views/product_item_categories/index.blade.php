242b9977b79a36fe7889fc53799106cc3ea6c4d3
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2025-09-06 11:40:25 +0700

    feat: fixing some issues

--- resources/views/product_item_categories/index.blade.php
+++ resources/views/product_item_categories/index.blade.php
@@ -3,64 +3,64 @@
 ])
 
 @section('content')
-    <div class="page-header">
-        <h3 class="page-title"> Page {{ $title }} </h3>
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{ route('product_item_category.index') }}">{{ $title }}</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Data List</li>
-            </ol>
-        </nav>
-    </div>
+  <div class="page-header">
+    <h3 class="page-title"> Page {{ $title }} </h3>
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('product_item_category.index') }}">{{ $title }}</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Data List</li>
+      </ol>
+    </nav>
+  </div>
 
-    <div class="col-lg-12 grid-margin stretch-card">
-        <div class="card">
-            <div class="card-body table-responsive">
-                <div class="row mb-2">
-                    <div class="col-md-4 mb-2">
-                        <form method="get">
-                          <input type="text" class="form-control" name="name" placeholder="Search name item kategori" value="{{ request('name') }}">
-                        </form>
-                    </div>
-                     <div class="col-md-8 text-lg-end">
-                         <a href="{{ $createLink }}" class="btn btn-primary">Create data</a>
-                     </div>
-                </div>
-                <table class="table table-bordered table-hover">
-                    <thead>
-                        <tr>
-                            <th> # </th>
-                            <th> Name </th>
-                            <th> Product </th>
-                            <th> Action </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($productItemCategories as $index => $product_item_category)
-                        <tr>
-                            <td>{{ $productItemCategories->firstItem() + $index }}</td>
-                            <td>{{ $product_item_category->name }}</td>
-                            <td>{{ $product_item_category->product->name }}</td>
-                            <td>
-                              @include('master.action', [
-                                  'view_url' => route('product_item_category.show', $product_item_category),
-                                  'edit_url' => route('product_item_category.edit', $product_item_category),
-                                  'delete_url' => route('product_item_category.destroy', $product_item_category),
-                              ])
-                            </td>
-                        </tr>
-                        @empty
-                        <tr>
-                            <td colspan="100%" class="text-center">No Data</td>
-                        </tr>
-                        @endforelse
-                    </tbody>
-                </table>
-                <div class="mt-2">
-                    {!! $productItemCategories->appends(request()->query())->links() !!}
-                </div>
-            </div>
+  <div class="col-lg-12 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body table-responsive">
+        <div class="row mb-2">
+          <div class="col-md-4 mb-2">
+            <form method="get">
+              <input type="text" class="form-control" name="name" placeholder="Search item category name" value="{{ request('name') }}">
+            </form>
+          </div>
+          <div class="col-md-8 text-lg-end">
+            <a href="{{ $createLink }}" class="btn btn-primary">Create data</a>
+          </div>
         </div>
+        <table class="table table-bordered table-hover">
+          <thead>
+          <tr>
+            <th> # </th>
+            <th> Name </th>
+            <th> Product </th>
+            <th> Action </th>
+          </tr>
+          </thead>
+          <tbody>
+          @forelse ($productItemCategories as $index => $product_item_category)
+            <tr>
+              <td>{{ $productItemCategories->firstItem() + $index }}</td>
+              <td>{{ $product_item_category->name }}</td>
+              <td>{{ $product_item_category->product->name }}</td>
+              <td>
+                @include('master.action', [
+                    'view_url' => route('product_item_category.show', $product_item_category),
+                    'edit_url' => route('product_item_category.edit', $product_item_category),
+                    'delete_url' => route('product_item_category.destroy', $product_item_category),
+                ])
+              </td>
+            </tr>
+          @empty
+            <tr>
+              <td colspan="100%" class="text-center">No Data</td>
+            </tr>
+          @endforelse
+          </tbody>
+        </table>
+        <div class="mt-2">
+          {!! $productItemCategories->appends(request()->query())->links() !!}
+        </div>
+      </div>
     </div>
+  </div>
 @endsection
 
