1b021c626c1a536edcae943b7df58ccca8bee82d
Yeri <mail@yeripratama.com>
2025-09-14 07:37:57 +0700

    fix: translate

--- resources/views/flash_sales/create.blade.php
+++ resources/views/flash_sales/create.blade.php
@@ -44,7 +44,7 @@
                 <tr>
                   <th style="width:36px;"></th>
                   <th>Item</th>
-                  <th class="text-end">Harga Normal</th>
+                  <th class="text-end">Normal Price</th>
                   <th style="width:160px;">Flash Price</th>
                   <th style="width:140px;">Stock</th>
                 </tr>
