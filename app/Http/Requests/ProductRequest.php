e136f95d2bc6541670ada849835cfa90bc1795c7
Yeri <mail@yeripratama.com>
2025-09-16 21:45:02 +0700

    feat: make input format nullable

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -32,7 +32,7 @@ class ProductRequest extends FormRequest
                     'product_category_id' => 'required',
                     'description' => 'required',
                     'how_to_order' => 'required',
-                    'input_format' => 'required',
+                    'input_format' => 'nullable',
                     'status' => 'required',
                     'picture' => 'required|image|mimes:jpeg,png,jpg,gif,svg,webp|max:5120',
                     'cover' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg,webp|max:5120',
