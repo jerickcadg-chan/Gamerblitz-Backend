09f109df77e90e5c1af47b31509260d9404802f1
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2025-09-21 20:44:00 +0700

    fix: logo login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
           <div class="col-lg-4 mx-auto">
             <div class="auth-form-light text-left p-5">
               <div class="text-center">
-                <img src="{{ asset('img/gpds_logo.png') }}" alt="header image" class="w-75 text-center">
+                <img src="{{ get_logo() }}" alt="header image" class="w-75 text-center">
               </div>
               <form class="pt-3" action="{{ route('login') }}" method="POST">
                 @csrf
