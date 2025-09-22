4f2f9b1608add6357d4aaa58aeb18a13f514f340
Yeri <mail@yeripratama.com>
2025-09-22 10:42:35 +0700

    feat: gpds game image seedd

--- app/Console/Commands/SeedGpdsGames.php
+++ app/Console/Commands/SeedGpdsGames.php
@@ -6,30 +6,36 @@ use App\Models\Product;
 use App\Models\ProductCategory;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 use League\Csv\Reader;
 use Illuminate\Support\Str;
 
-class SeedGpdsProducts extends Command
+class SeedGpdsGames extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'app:seed-gpds-products';
+    protected $signature = 'app:seed-gpds-games';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Seed gpds products from gpds-games.csv';
+    protected $description = 'Seed gpds games from gpds-games.csv';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
+        $imageMap = json_decode(
+            file_get_contents(resource_path('data/gpds-games-images.json')),
+            true
+        );
         $csv = Reader::createFromPath(resource_path('data/gpds-games.csv'), 'r');
 
         $productInserted = 0;
@@ -50,7 +56,9 @@ class SeedGpdsProducts extends Command
 
             foreach ($csv as $i => $record) {
                 // skip header
-                if ($i === 0) continue;
+                if ($i === 0) {
+                    continue;
+                }
 
                 $name = $record[0];
                 $code = $record[1];
@@ -95,6 +103,16 @@ class SeedGpdsProducts extends Command
                     $categoryInserted++;
                 }
                 if ($product->wasRecentlyCreated) {
+                    $imageUrl = $imageMap[$name];
+                    $response = Http::get($imageUrl);
+                    if ($response->successful()) {
+                        $path = "image/" . $product->slug . '.' . pathinfo($imageUrl, PATHINFO_EXTENSION);
+                        Storage::put($path, $response->body());
+                        $product->default_picture = $path;
+                        $product->save();
+                    } else {
+                        echo "Failed to fetch image for $name -> $imageUrl";
+                    }
                     $productInserted++;
                 }
                 // print_r($product->toArray());
@@ -102,7 +120,7 @@ class SeedGpdsProducts extends Command
 
             DB::commit();
 
-            echo "Inserted $productInserted new products and $categoryInserted new categories.";
+            echo "Inserted $productInserted new games and $categoryInserted new categories.";
         } catch (\Exception $e) {
             echo $e->getMessage();
             DB::rollBack();
