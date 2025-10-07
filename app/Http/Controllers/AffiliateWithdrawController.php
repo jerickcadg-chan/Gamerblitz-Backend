105da2c9156e2a55149a0398c89ed96286f70f9c
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2025-10-07 08:31:56 +0700

    feat: affiliate withdraw fix

--- app/Http/Controllers/AffiliateWithdrawController.php
+++ app/Http/Controllers/AffiliateWithdrawController.php
@@ -59,7 +59,7 @@ class AffiliateWithdrawController extends Controller
             // Save history
             $affiliate->affiliateHistories()->create([
                 'affiliate_id'       => $affiliate->id,
-                'affiliateable_type' => get_class($affiliateWithdraw),
+                'affiliateable_type' => 'App\Models\AffiliateWithdraw',
                 'affiliateable_id'   => $affiliateWithdraw->id,
                 'amount'             => -$affiliateWithdraw->amount,
                 'amount_before'      => $amountBefore,
