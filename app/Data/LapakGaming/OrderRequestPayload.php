64d4c0a543cb60b8e4203e51ed9bbf22164fc96a
Yeri <mail@yeripratama.com>
2025-10-07 12:13:51 +0700

    fix: optional product code

--- app/Data/LapakGaming/OrderRequestPayload.php
+++ app/Data/LapakGaming/OrderRequestPayload.php
@@ -9,7 +9,7 @@ class OrderRequestPayload extends Data
     public function __construct(
         // mandatory
         public int $count_order,
-        public string $product_code, // mandatory if group_product empty
+        public string $product_code = '', // mandatory if group_product empty
         public string $group_product = '', // mandatory if product_code empty
 
         // mandatory/optional
