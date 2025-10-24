4d81e648abd1b4b052b57038b46c37fa14b2f83b
Sofronius Ruddy <ruddy@circlecreative.dev>
2025-10-24 10:46:05 +0700

    feat: cryptomus service

---
+++ app/Services/CryptomusService.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Deposit;
+use App\Models\Order;
+use App\Models\Setting;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Exception;
+
+class CryptomusService
+{
+    /**
+     * @param Order $order
+     * 
+     * @return array
+     */
+    public function createOrderCryptomusInvoice(Order $order): array
+    {
+        $payload = [
+            'amount'       => $order->total_price,
+            'currency'     => $order->paymentMethod->currency,
+            'order_id'     => $order->code,
+            'url_return'   => $order->payment_url_full,
+            'url_success'  => $order->payment_url_full,
+            'url_callback' => route('callback.cryptomus'),
+        ];
+
+        $response = Http::withHeaders([
+            'merchant' => Setting::getByKey('cryptomus_merchant_id'),
+            'sign'     => md5(
+                base64_encode(json_encode($payload)) .
+                Setting::getByKey('cryptomus_api_Key')
+            ),
+        ])->post(
+            Setting::getByKey('cryptomus_api_url') . '/payment',
+            $payload
+        );
+
+        $json = $response->json();
+
+        Log::info('Cryptomus payment request response', $json);
+
+        if ($response->failed()) {
+            throw new Exception('Failed to create payment: ' . ($json['message'] ?? 'Unknown error'));
+        }
+
+        $order->update([
+            'payment_url'  => $json['result']['url'] ?? null,
+            'payment_code' => $json['result']['order_id'] ?? null,
+            'payment_id'   => $json['result']['uuid'] ?? null,
+        ]);
+
+        return $json;
+    }
+    
+    /**
+     * @param Deposit $deposit
+     * 
+     * @return array
+     */
+    public function createDepositCryptomusInvoice(Deposit $deposit): array
+    {
+        $payload = [
+            'amount'       => ceil($deposit->total_amount),
+            'currency'     => $deposit->paymentMethod->currency,
+            'order_id'     => $deposit->code,
+            'url_return'   => config('app.fe_url') . '/dashboard/deposit/' . $deposit->code,
+            'url_success'  => config('app.fe_url') . '/dashboard/deposit/' . $deposit->code,
+            'url_callback' => route('callback.cryptomus'),
+        ];
+
+        $response = Http::withHeaders([
+            'merchant' => Setting::getByKey('cryptomus_merchant_id'),
+            'sign'     => md5(
+                base64_encode(json_encode($payload)) .
+                Setting::getByKey('cryptomus_api_Key')
+            ),
+        ])->post(
+            Setting::getByKey('cryptomus_api_url') . '/payment',
+            $payload
+        );
+
+        $json = $response->json();
+
+        Log::info('Cryptomus deposit request response', $json);
+
+        if ($response->failed()) {
+            throw new Exception('Failed to create payment: ' . ($json['message'] ?? 'Unknown error'));
+        }
+
+        $deposit->update([
+            'payment_url'  => $json['result']['url'] ?? null,
+            'payment_code' => $json['result']['order_id'] ?? null,
+            'payment_id'   => $json['result']['uuid'] ?? null,
+        ]);
+
+        return $json;
+    }
+}
